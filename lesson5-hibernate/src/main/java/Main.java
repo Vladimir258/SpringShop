c85b1df4efe4d1953be9fd98ccda0aca843faa7d
Vladimir <fominsky_v@live.com>
2022-09-01 02:36:03 +0500

    Материалы 5-ого урока

--- lesson5-hibernate/src/main/java/Main.java
+++ lesson5-hibernate/src/main/java/Main.java
@@ -1,15 +1,64 @@
 import org.hibernate.cfg.Configuration;
+import ru.fominskiy.model.User;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityManager;
-
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory entityManagerFactory =   new Configuration()
+        EntityManagerFactory entityManagerFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .buildSessionFactory();
 
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
+        // INSERT
+//        entityManager.getTransaction().begin();
+//        entityManager.persist(new User("User1", "1@a.com", "pass1"));
+//        entityManager.persist(new User("User2", "2@a.com", "pass2"));
+//        entityManager.persist(new User("User3", "3@a.com", "pass3"));
+//        entityManager.getTransaction().commit();
+
+        // SELECT
+//        User user = entityManager.find(User.class, 1L);
+//        System.out.println(user.getUsername());
+
+        // JPQL, HQL
+                // List<User> users = entityManager.createQuery("select u from User u", User.class).getResultList();
+//        List<User> users = entityManager.createQuery("select u from User u where u.id in (1, 2)", User.class).getResultList();
+//        for (User u: users) {
+//            System.out.println(u.getUsername());
+//        }
+
+        // UPDATE v1
+//        entityManager.getTransaction().begin();
+//        User user = entityManager.find(User.class, 1L);
+//        user.setUsername("new Username");
+//        entityManager.getTransaction().commit();
+//        entityManager.close();
+
+        // UPDATE v2
+//        entityManager.getTransaction().begin();
+//        User user = new User("User2Second", "2@a.com", "pass2");
+//        user.setId(2L);
+//        entityManager.merge(user);
+//        entityManager.getTransaction().commit();
+//        entityManager.close();
+
+        // DELETE
+//        entityManager.getTransaction().begin();
+//        // 1 способ JPQL
+//        //entityManager.createQuery("delete from User u where u.id = 3").executeUpdate();
+//
+//        // 2 способ
+//        User user = entityManager.find(User.class, 2L);
+//        entityManager.remove(user);
+//
+//        entityManager.getTransaction().commit();
+//        entityManager.close();
+
+
+        entityManagerFactory.close();
     }
 }
---
+++ lesson5-hibernate/src/main/java/ru/fominskiy/model/User.java
@@ -0,0 +1,30 @@
+package ru.fominskiy.model;
+
+import javax.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false, length = 1024)
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
